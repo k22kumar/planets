fb1c9c1e8071748174b6afd8014f1e7756d2b443
Kajanth <kajanthkumar@gmail.com>
2022-04-11 15:33:12 -0400

    simple script to find habitable planets

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const { parse } = require('csv-parse');
 const fs = require('fs');
 
-const results = [];
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+    // koi disposition is if its been observed by kepler, koi_insol is amount of stellar flux, koi_prad
+    return planet['koi_disposition'] === 'CONFIRMED'
+        && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+        && planet['koi_prad'] < 1.6
+}
 
 fs.createReadStream('kepler_data.csv')
+.pipe(parse({
+    // treat lines that start with this character as comments
+    comment: '#',
+    // return each row as a JS object with key value pairs
+    columns: true
+}))
     .on('data', (data) => {
-        results.push(data);
+        if (isHabitablePlanet(data)) habitablePlanets.push(data);
     })
     .on('error', (err) => {
         console.log(err);
     })
     .on('end', () => {
-        console.log(results);
-        console.log('done');
+        console.log(`${habitablePlanets.length} habitable planets found!`);
     });
 parse();
